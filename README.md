49776736bbb74d2c0f299389727aeca55db655db
ATAKAN SOLAK <atakansolak28@hotmail.com>
2023-06-16 22:11:10 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,62 @@
+<center>
+
+# **Spotify OCR Automation**
+
+</center>
+
+This program, designed with the purpose of selecting songs added by Spotify when the enhancement feature is turned on in your playlists, employs OCR (Optical Character Recognition) to automate interactions with the Spotify application. It captures a screenshot of the Spotify window, performs text recognition to locate specific elements, and simulates mouse clicks to interact with these identified elements.
+
+# Prerequisites
+- Python 3.7 or above
+- Tesseract OCR Engine installed (https://github.com/tesseract-ocr/tesseract)
+# Installation
+1. Clone the repository:
+
+```
+git clone https://github.com/mapleleafu/spotify_enhanced
+```
+2. Change into the project directory:
+
+```
+spotify_enhanced
+```
+
+3. Install the required dependencies:
+
+```
+pip install -r requirements.txt
+```
+4. Set up the environment variables:
+
+- Create a `.env` file in the project directory.
+
+- Open the `.env` file and add the following line, replacing `<path_to_tesseract_executable>` with the actual path to the Tesseract executable file on your system:
+
+```
+PATH_TES=<path_to_tesseract_executable>
+```
+5. Run the program:
+
+```
+python main.py
+```
+# Usage
+1. Make sure Spotify is running and visible on your screen.
+
+2. The program will automatically locate the Spotify window and capture a screenshot.
+
+3. It will perform OCR on the screenshot to identify elements matching the pattern defined in the code.
+
+4. If the pattern is found, the program will simulate mouse clicks on the identified elements.
+
+5. The process will continue until the pattern is not found for two consecutive iterations.
+
+6. Press any key to stop the program.
+
+# Customization
+
+- You can modify the `pixel_coordinates` variable to define the pixel coordinates of a specific area within the Spotify window to capture in the screenshot. Adjust it according to your requirements.
+
+- Update the `pattern` variable to define a different pattern to match elements in the Spotify window. This pattern is used to filter out non-relevant elements.
+
+- Modify the `wx` and `wy` variables to adjust the screen position for mouse clicks. These variables represent the offset from the top-left corner of the Spotify window.
---
+++ main.py
@@ -0,0 +1,110 @@
+import cv2
+import re
+import pyautogui
+import win32gui
+import keyboard
+import os
+from pytesseract import pytesseract, Output
+from PIL import ImageGrab
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#! Define Tesseract path here or in the .env file
+path_tes = os.getenv("PATH_TES")
+pytesseract.tesseract_cmd = path_tes
+
+toplist, winlist = [], []
+pixel_coordinates = (1300, 0, 1420, 0) #! Define pixel coordinates of the "Added by" column
+
+def enum_cb(hwnd, results):
+    winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+win32gui.EnumWindows(enum_cb, [])
+
+def bring_spotify_to_foreground(pixel_coordinates):
+    spotify_app = [(hwnd, title) for hwnd, title in winlist if 'spotify' in title.lower()]
+    if spotify_app:
+        hwnd = spotify_app[0][0]
+        win32gui.SetForegroundWindow(hwnd)
+        bbox = win32gui.GetWindowRect(hwnd)
+        img = ImageGrab.grab(bbox)
+        pixel_coordinates = (pixel_coordinates[0], pixel_coordinates[1], pixel_coordinates[2], img.size[1])
+        cropped_img = img.crop(pixel_coordinates)
+        cropped_img.save('cropped_screenshot.png')
+    else:
+        print("Spotify app not found.")
+
+img_path = 'cropped_screenshot.png'
+
+# Define a pattern that closely matches 'Spotify'
+pattern = re.compile('s[pb]o[t1l]i?[tf][iy]', re.IGNORECASE)
+
+spotify_not_found_count = 0
+
+
+running = True
+stop_key = None
+
+# Define a callback for any key
+def on_any_key_press(e):
+    global running, stop_key
+    if e.name.isalnum():
+        running = False
+        stop_key = e.name
+
+
+keyboard.on_press(on_any_key_press)
+
+while running:
+    bring_spotify_to_foreground(pixel_coordinates)
+
+    img = cv2.imread(img_path)  
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Perform OCR on the image
+    image_data = pytesseract.image_to_data(img, output_type=Output.DICT, config='--oem 3 --psm 6')
+
+    coordinates = []
+
+    found_spotify = False  # Variable to check if 'Spotify' was found in the current iteration
+
+    for i, word in enumerate(image_data['text']):
+        if pattern.match(word):
+            x, y, w, h = image_data['left'][i], image_data['top'][i], image_data['width'][i], image_data['height'][i]
+            coordinates.append((x, y, w, h))
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.putText(img, word, (x - 60, y - 16), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
+            print("Predicted text:", word)
+            found_spotify = True  
+
+    if not found_spotify:
+        spotify_not_found_count += 1
+    else:
+        spotify_not_found_count = 0
+
+    if spotify_not_found_count >= 2:
+        print("Spotify text not found after 2 scrolls. Stopping the program.")
+        break
+
+    # Sort the coordinates from top to bottom
+    coordinates.sort(key=lambda x: x[1])
+
+    
+    wx, wy = 1300, 0  #! Replace this according to your Spotify window's position
+
+    for (x, y, w, h) in coordinates:
+        screen_x = wx + x
+        screen_y = wy + y
+        pyautogui.moveTo(screen_x, screen_y)
+        pyautogui.keyDown('ctrl')
+        pyautogui.click()
+        pyautogui.keyUp('ctrl')
+
+    pyautogui.scroll(-1000)
+
+    cv2.imwrite('annotified_image.png', img)
+    pass  
+
+print(f"Program stopped after pressing '{stop_key}' key.")
