cdd44935ebca481a2479f4240a3bed0e0c48fd08
ATAKAN SOLAK <atakansolak28@hotmail.com>
2023-06-17 19:12:55 +0300

    feat: added logo recognition

--- main.py
+++ main.py
@@ -1,83 +1,67 @@
 import cv2
-import re
+import numpy as np
 import pyautogui
 import win32gui
 import keyboard
-import os
-from pytesseract import pytesseract, Output
 from PIL import ImageGrab
 from dotenv import load_dotenv
 
 load_dotenv()
 
-#! Define Tesseract path here or in the .env file
-path_tes = os.getenv("PATH_TES")
-pytesseract.tesseract_cmd = path_tes
-
 toplist, winlist = [], []
-pixel_coordinates = (1300, 0, 1470, 0) #! Define pixel coordinates of the "Added by" column
 
 def enum_cb(hwnd, results):
     winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
 win32gui.EnumWindows(enum_cb, [])
 
-def bring_spotify_to_foreground(pixel_coordinates):
+def bring_spotify_to_foreground():
     spotify_app = [(hwnd, title) for hwnd, title in winlist if 'spotify' in title.lower()]
     if spotify_app:
         hwnd = spotify_app[0][0]
         win32gui.SetForegroundWindow(hwnd)
         bbox = win32gui.GetWindowRect(hwnd)
         img = ImageGrab.grab(bbox)
-        pixel_coordinates = (pixel_coordinates[0], pixel_coordinates[1], pixel_coordinates[2], img.size[1])
-        cropped_img = img.crop(pixel_coordinates)
-        cropped_img.save('cropped_screenshot.png')
-    else:
-        print("Spotify app not found.")
+        img.save('screenshot.png')
 
-img_path = 'cropped_screenshot.png'
-
-# Define a pattern that closely matches 'Spotify'
-pattern = re.compile('s[pb]?o?[t1l]?i?[tf]?[iy]?|sin|sity|ss', re.IGNORECASE)
+img_path = 'screenshot.png'
 
 spotify_not_found_count = 0
-
 
 running = True
 stop_key = None
 
-# Define a callback for any key
 def on_any_key_press(e):
     global running, stop_key
     if e.name.isalnum():
         running = False
         stop_key = e.name
 
-
 keyboard.on_press(on_any_key_press)
 
 while running:
-    bring_spotify_to_foreground(pixel_coordinates)
+    bring_spotify_to_foreground()
 
-    img = cv2.imread(img_path)  
-
-    # Convert the image to grayscale
+    img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('spotify_logo.png',0)
 
-    # Perform OCR on the image
-    image_data = pytesseract.image_to_data(img, output_type=Output.DICT, config='--oem 3 --psm 6')
+    res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where(res >= threshold)
 
+    found_spotify = False
     coordinates = []
+    min_distance_pct = 0.1
+    min_distance = img.shape[1] * min_distance_pct
 
-    found_spotify = False  # Variable to check if 'Spotify' was found in the current iteration
+    w, h = template.shape[::-1]
 
-    for i, word in enumerate(image_data['text']):
-        if pattern.match(word):
-            x, y, w, h = image_data['left'][i], image_data['top'][i], image_data['width'][i], image_data['height'][i]
-            coordinates.append((x, y, w, h))
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
-            cv2.putText(img, word, (x - 60, y - 16), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
-            print("Predicted text:", word)
-            found_spotify = True  
+    for pt in zip(*loc[::-1]):
+        if not any(np.sqrt((pt[0] - x)**2 + (pt[1] - y)**2) < min_distance for (x, y) in coordinates):
+            cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
+            print("Logo found at: ", pt)
+            coordinates.append(pt)
+            found_spotify = True
 
     if not found_spotify:
         spotify_not_found_count += 1
@@ -85,27 +69,19 @@ while running:
         spotify_not_found_count = 0
 
     if spotify_not_found_count >= 5:
-        print("Spotify text not found after 5 scrolls. Stopping the program.")
         break
 
-    # Sort the coordinates from top to bottom
-    coordinates.sort(key=lambda x: x[1])
 
-    
-    wx, wy = 1300, 0  #! Replace this according to your Spotify window's position
+    # for (x, y) in coordinates:
+    #     screen_x = x
+    #     screen_y = y
+    #     pyautogui.moveTo(screen_x, screen_y)
+    #     pyautogui.keyDown('ctrl')
+    #     pyautogui.click()
+    #     pyautogui.keyUp('ctrl')
 
-    for (x, y, w, h) in coordinates:
-        screen_x = wx + x
-        screen_y = wy + y
-        pyautogui.moveTo(screen_x, screen_y)
-        pyautogui.keyDown('ctrl')
-        pyautogui.click()
-        pyautogui.keyUp('ctrl')
-
-    #! Replace this with the total pixel amount required to scroll through an entire page
     pyautogui.scroll(-980)
-    cv2.imwrite('annotified_image.png', img)
-    pass  
+    cv2.imwrite('annotated_image.png', img)
 
 if stop_key != None:
     print(f"Program stopped after pressing '{stop_key}' key.")
